3a90dfcd7850290f6be8d9433a01b611b5730d28
Ernest <ernesto170696@gmail.com>
2022-01-19 00:00:47 +0300

    add Leson 4

--- Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
+++ Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveCommandExecutor : CommandExecutorBase<IMoveCommand>
 {
-    public override void ExecuteSpecificCommand(IMoveCommand command)
+    [SerializeField] private UnitMovementStop _stop;
+    [SerializeField] private Animator _animator;
+
+    public async override void ExecuteSpecificCommand(IMoveCommand command)
     {
-        Debug.Log($"{name} is moving to {command.Target}!");
+        GetComponent<NavMeshAgent>().destination = command.Target;
+        _animator.SetTrigger("Walk");
+        await _stop;
+        _animator.SetTrigger("Idle");
 
     }
 }
